32648a633a187cb71bd4c5fc9d5e952eb14dddad
장혜원 <127714800+janghw0126@users.noreply.github.com>
2024-01-07 13:58:36 +0900

    2024-01-04

--- janghw0126/구현/Implementation_1.py
+++ janghw0126/구현/Implementation_1.py
@@ -1,6 +1,7 @@
 sentence=" The first character is a blank"
+print(sentence.split(" "))
 
 #문장을 공백을 기준으로 나누고 빈 문자열을 제외한다.
 word=[word for word in sentence.split(" ") if word]
 
-print(len(word)) 
+print(len(word))
