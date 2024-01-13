cb0e40e5842ca4345272344a223350b079b21753
Hwang_yerin <lucialu02@naver.com>
2024-01-13 23:48:16 +0900

    2024-01-13
    .

--- Hwangyerin/정수론/약수들의 합.py
+++ Hwangyerin/정수론/약수들의 합.py
@@ -7,6 +7,7 @@ while True:
         for j in range(1, num//2 + 1):
             if num%j == 0:
                 L.append(j)
+                
         if sum(L) == num:
             print(num, end=' = ')
             print(*L,sep=' + ')
