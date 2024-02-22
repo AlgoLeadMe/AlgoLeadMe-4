33eaf3a8fad7bc9c3488d6bfe6d27c115b5bd598
장혜원 <127714800+janghw0126@users.noreply.github.com>
2024-02-22 16:52:48 +0900

    2024-02-21

--- janghw0126/백트래킹/Backtracking_4.py
+++ janghw0126/백트래킹/Backtracking_4.py
@@ -1,6 +1,9 @@
 import sys
 input = sys.stdin.readline
 
+n, m = map(int, input().split())
+ans = []
+
 #재귀 함수를 호출한다.
 def dfs():
     #만약 리스트의 크기가 m이라면 모두 고른 것이 되므로 ans 리스트의 요소들을 출력한다.
@@ -16,7 +19,5 @@ def dfs():
         #함수 호출 후에는 해당 자릿수에서의 선택이 끝났으므로, ans 리스트에서 마지막에 추가된 숫자를 제거한다.-> 다음 자릿수의 반복문을 실행해야 되므로
         ans.pop()
 
-n, m = map(int, input().split())
-ans = []
 #dfs함수를 이용해서 모든 경우의 수를 찾는다.
 dfs()
