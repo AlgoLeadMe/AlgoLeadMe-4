4b0ba276e49407af2a05b74916cedaed924db998
Hwang_yerin <50723984+Hwangyerin@users.noreply.github.com>
2024-02-24 20:30:51 +0900

    2024-02-24
    update 풍선 터뜨리기.py

--- Hwangyerin/덱/풍선 터뜨리기.py
+++ Hwangyerin/덱/풍선 터뜨리기.py
@@ -4,11 +4,11 @@ input = sys.stdin.readline
 
 # 입력 받기
 balloon_count = int(input())
-balloon_list = deque(enumerate([int(x) for x in input().split()]))
+balloon_list = deque(enumerate([int(x) for x in input().split()],start=1))
 result = []
 
 idx, paper_number = balloon_list.popleft()
-result.append(idx + 1)
+result.append(idx)
 
 # 풍선 터뜨리는 과정
 while balloon_list:
@@ -20,7 +20,7 @@ while balloon_list:
         for _ in range(abs(paper_number)):
             balloon_list.appendleft(balloon_list.pop())
     idx, paper_number = balloon_list.popleft()
-    result.append(idx + 1)
+    result.append(idx)
 
 print(*result)
 
