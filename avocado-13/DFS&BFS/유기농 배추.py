3683f546e61b587d571d8682af77d4cc8ff23555
avocado-13 <sihyeon4785@gmail.com>
2024-02-29 20:39:01 +0900

    Update 유기농 배추.py

--- avocado-13/DFS&BFS/유기농 배추.py
+++ avocado-13/DFS&BFS/유기농 배추.py
@@ -39,7 +39,7 @@
 #                 answer += 1
 
 #     print(answer)
-#--------------------------------------------------------
+# --------------------------------------------------------
 
 # 2. graph 배열만 활용한 풀이
 import sys
@@ -55,7 +55,7 @@ def dfs(x,y):
     for dirIdx in range(4):
         newX = x + dirR[dirIdx]
         newY = y + dirC[dirIdx]
-        if graph[newX][newY] and not visited[newX][newY]:
+        if graph[newX][newY] :
             dfs(newX,newY)
 
 
@@ -68,12 +68,12 @@ for _ in range(T):
     # 1. 그래프 정보 입력
     for _ in range(K):
         x, y = map(int,input().split())
-        graph[y+1][x+1] = True #(1,1) 부터 시작하는 것이 range 체크를 안해도 되어서 간결하다. 
+        graph[x+1][y+1] = True #(1,1) 부터 시작하는 것이 range 체크를 안해도 되어서 간결하다. 
     
     # 2, 방문하지 않은 지점부터 dfs 풀기 
     answer = 0 
-    for i in range(1, N+1):
-        for j in range(1, M+1):
+    for i in range(1, M+1):
+        for j in range(1, N+1):
             if graph[i][j]:
                 dfs(i,j)
                 answer += 1
