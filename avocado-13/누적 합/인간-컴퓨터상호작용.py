38678186f0bb4876e869caeee1c8c5a24e3063b2
avocado-13 <sihyeon4785@gmail.com>
2024-03-05 23:55:13 +0900

    Update 인간-컴퓨터상호작용.py

--- avocado-13/누적 합/인간-컴퓨터상호작용.py
+++ avocado-13/누적 합/인간-컴퓨터상호작용.py
@@ -1,34 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-S = list(input().rstrip())
+S = input().rstrip()
 q = int(input())
 
-prefix = []
+# 각 인덱스별로 해당 문자가 몇 번 등장했는지를 누적합으로 저장하는 리스트를 생성합니다.
+prefix = [0] * len(S)
 
+# 누적합을 계산하는 함수를 정의합니다.
 def prefixSum():
-    global alpha
-    global lt 
-    global rt
-
-    # 누적합 리스트 세팅
     if S[0] == alpha:
-       prefix[0] = 1
+        prefix[0] = 1
     else :
-       prefix[0] = 0
-    for i in range(1,len(S)):
-        prefix[i] = prefix[i-1] + checkChar(S[i])
+        prefix[0] = 0
+    for i in range(1, len(S)):
+        prefix[i] = prefix[i - 1] + (S[i] == alpha)
 
-def checkChar(s):
-    if s == alpha :
-        return 1
-    else :
-        return 0
-
-
-
-for i in range(q):
-   alpha, lt, rt = input().split()
-   # 누적합 함수 실행 
-   print(prefix[int(rt)-1]-prefix[int(lt)-1])
+# 주어진 질문에 대해 처리합니다.
+for _ in range(q):
+    alpha, lt, rt = input().split()
+    lt, rt = int(lt), int(rt)
+    prefixSum()
     
+    # 구간 [lt, rt] 내에 해당 알파벳이 몇 번 등장했는지를 계산합니다.
+    # lt - 1과 rt 인덱스의 값을 이용하여 첫 번째와 마지막 문자를 포함합니다.
+    print(prefix[rt] - prefix[lt] + (S[lt]==alpha))
