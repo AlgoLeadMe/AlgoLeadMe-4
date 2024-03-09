4cd92016b0154ae96fa59f126fe4141f21c92990
avocado-13 <sihyeon4785@gmail.com>
2024-03-10 01:19:57 +0900

    Update Cow line.py

--- avocado-13/큐/Cow line.py
+++ avocado-13/큐/Cow line.py
@@ -3,9 +3,9 @@ from collections import deque
 
 input = sys.stdin.readline
 queue = deque()
-
+cowNum = 1 
 N = int(input()) # 줄에 설 소가 몇마리인지 입력받기
-for cowNum in range(1,N+1):
+for _ in range(N):
     cmd = input().split()
     action, side = cmd[0], cmd[1]
     if action == "A":
@@ -13,13 +13,15 @@ for cowNum in range(1,N+1):
             queue.appendleft(cowNum)
         else:
             queue.append(cowNum)
+        cowNum += 1 
     else : # action == "D"
         K = cmd[2]
         if side == "L":
             for _ in range(int(K)):
                 queue.popleft()
+               
         else:
             for _ in range(int(K)):
                 queue.pop()
 
-print(queue,sep="\n")
+print(*queue,sep="\n")
