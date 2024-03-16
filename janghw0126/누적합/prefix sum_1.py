836c1b8fd8bdc018b836c079f9f8a40dd709ea67
장혜원 <127714800+janghw0126@users.noreply.github.com>
2024-03-17 02:09:28 +0900

    2024-03-15

--- janghw0126/누적합/prefix sum_1.py
+++ janghw0126/누적합/prefix sum_1.py
@@ -1,13 +1,14 @@
 import sys
 input = sys.stdin.readline
+
 # 각각의 수들을 입력받는다.
 n,k,q,m = map(int, input().split())
-# 학생들이 졸고 있는지의 여부를 표시하기 위해 처음에는 모두 깨어있음으로 초기화한다.
+
+# 졸고 있는 학생들과 출석 코드를 받을 학생들을 초기화한다.
 sleep = [0]*(n+3)
-# 학생들의 출석 여부를 표시하기 위해 처음에는 모두 결석으로 설정한다.
 check = [0]*(n+3)
 
-# 졸고 있는 학생의 입장 번호들을 입력받아 1로 바꾼다.
+# 졸고 있는 학생의 입장 번호들을 입력 받아 1로 바꾼다.
 for i in map(int, input().split()):
     sleep[i] = 1
 # 출석 코드를 입력할 학생들의 번호를 반복한다.
@@ -22,12 +23,11 @@ for i in map(int, input().split()):
 
 prefix = [check[0]]
 for i in range(1, n+3):
-    # 각 번호 i까지 출석한 학생들의 누적합을 구한다.
+    # 각 번호 i까지 출석한 학생들의 합을 구한다.
     prefix.append(prefix[-1]+check[i])
 
-# 쿼리를 처리하는 부분이다.
 for _ in range(m):
-    # s부터 e까지 결석한 전체 학생들의 합을 구한다.
+    # s부터 e까지 결석한 학생들의 합을 구한다.
     s, e = map(int, input().split()) 
-    # 전체 - (s부터 e까지 출석한 학생들), 즉 결석한 학생의 수를 출력해준다.
+    # 전체 - (s부터 e까지 출석한 학생들)을 출력해준다.
     print(e-s+1 - (prefix[e]-prefix[s-1])) 
