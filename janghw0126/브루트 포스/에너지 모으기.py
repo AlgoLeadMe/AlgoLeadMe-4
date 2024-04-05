7099ba58ee6dc7727ef7fab5bb1dec33a749d03b
장혜원 <127714800+janghw0126@users.noreply.github.com>
2024-04-05 15:42:00 +0900

    2024-04-03 에너지 모으기

--- janghw0126/브루트 포스/에너지 모으기.py
+++ janghw0126/브루트 포스/에너지 모으기.py
@@ -1,18 +1,26 @@
-def dfs(tmp):
+import sys
+
+N=int(input())
+
+energy_weight=list(map(int,input().split()))
+
+#최댓값을 갱신하는 변수를 선언한다.
+ans=0
+
+#최댓값 갱신해주는 재귀 함수를 선언한다.
+def max_energy(m):
     global ans
-    if len(a) == 2:
-        if ans < tmp:
-            ans = tmp
+    # 구슬이 두 개가 남았을 경우에 최댓값을 갱신해준다.
+    if len(energy_weight)==2:
+        ans=max(ans,m)
         return
-    else:
-        for i in range(1, len(a) - 1):
-            k = a[i]
-            del a[i]
-            dfs(tmp + a[i - 1] * a[i])
-            a.insert(i, k)
+    # 에너지의 합을 누적해서 구해준다.
+    for i in range(1,len(energy_weight)-1):
+        ball=energy_weight[i-1]*energy_weight[i+1]
+        v=energy_weight.pop(i)
+        max_energy(m+ball)
+        # 에너지의 최댓값을 갱신한 후에, 제거했던 에너지 구슬을 다시 삽입해준다.
+        energy_weight.insert(i,v)
 
-
-N, a = int(input()), list(map(int, input().split()))
-ans = 0
-dfs(0)
+max_energy(0)
 print(ans)
