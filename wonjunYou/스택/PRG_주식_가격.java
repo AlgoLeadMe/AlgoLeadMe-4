7f9c6d5c83d6af5b77c31f0183e81307c3b4afb0
wonjunYou <youwjune@gmail.com>
2024-07-21 01:47:54 +0900

    2024-07-17 리뷰 반영

--- wonjunYou/스택/PRG_주식_가격.java
+++ wonjunYou/스택/PRG_주식_가격.java
@@ -9,15 +9,12 @@ public class PRG_주식_가격 {
       int[] answer = new int[n];
 
       Stack<Integer> stack = new Stack<>();
-      stack.push(0);
 
-      for (int i = 1; i < n; i++) {
-
+      for (int i = 0; i < n; i++) {
         while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
           int j = stack.pop();
           answer[j] = i - j;
         }
-
         stack.push(i);
       }
 
