9b18122250fd8b5c6447da2196a4762385970413
jung0115 <jungim7490@gmail.com>
2024-07-28 00:05:31 +0900

    2024-07-27 주석 추

--- jung0115/자료 구조/Baekjoon_14725.kt
+++ jung0115/자료 구조/Baekjoon_14725.kt
@@ -28,22 +28,25 @@ fun main() {
     for(j: Int in 1..num) {
       val currentFeed = st.nextToken()
       var index = -1
+      // 부모 노드에 이미 존재하는지 확인
       for(k: Int in 0..parent.nodes.size - 1) {
         if(parent.nodes[k].feed.equals(currentFeed)) {
           index = k
           break
         }
       }
+      // 존재하지 않을 경우 자식으로 추가
       if(index == -1) {
         parent.nodes.add(Node(currentFeed, mutableListOf()))
         index = parent.nodes.size - 1
       }
       
+      // 다음 자식을 체크하기 위해 depth 증가
       parent = parent.nodes[index]
     }
   }
 
-  root.nodes.sortBy{ it.feed }
+  root.nodes.sortBy{ it.feed } // 자식 노드를 먹이 이름 기준으로 오름차순 정렬
   for(node in root.nodes) {
     tree(node, "")
   }
@@ -54,8 +57,8 @@ fun main() {
 fun tree(current: Node, depth: String) {
   answer.append(depth).append(current.feed).append("\n")
 
-  current.nodes.sortBy{ it.feed }
+  current.nodes.sortBy{ it.feed } // 자식 노드를 먹이 이름 기준으로 오름차순 정렬
   for(node in current.nodes) {
-    tree(node, depth + DEPTH_STRING)
+    tree(node, depth + DEPTH_STRING) // Depth 증가시켜주고 자식 노드도 탐색
   }
 }
