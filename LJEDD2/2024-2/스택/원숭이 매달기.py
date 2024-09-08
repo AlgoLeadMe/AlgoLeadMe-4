0e8b0149cc2f8f115becb6b538e9f0e035e9380f
ljedd2 <charzim0611@naver.com>
2024-09-08 20:22:13 +0900

    2024-09-08 원숭이 매달기.py
    문자열 풀이

--- LJEDD2/2024-2/스택/원숭이 매달기.py
+++ LJEDD2/2024-2/스택/원숭이 매달기.py
@@ -1,22 +1,37 @@
+# 더 간단한 풀이 ... (문자열)
 import sys
-
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-    s = input().rstrip()  # 괄호 문자열
+    cnt = 0
+    s = input().rstrip()
+    while "[]" in s:
+        s = s.replace("[]", "")
+        cnt += 1
 
-    max_depth = 0  # 최대 깊이를 저장할 변수
-    tree = list()  # 현재 열린 괄호를 저장할 스택임
+    print(2**cnt)
 
-    for i in s:
-        # 여는 괄호일 경우 스택에 열린 괄호 추가
-        if i == '[':
-            tree.append('[')
-            continue
-        # 닫는 괄호일 경우
-        max_depth = max(len(tree), max_depth)
-        # 현재 깊이와 최대 깊이 중 큰 값을 저장하고
-        tree.pop()  # 마지막 열린 괄호를 제거
 
-    print(2 ** max_depth)  # 쌍을 이룰때 가지 생성, 가지2-.1개씩 나눠가짐
-    # 결국엔 2의 depth승 
+
+# import sys
+
+# input = sys.stdin.readline
+
+# for _ in range(int(input())):
+#     s = input().rstrip()  # 괄호 문자열
+
+#     max_depth = 0  # 최대 깊이를 저장할 변수
+#     tree = list()  # 현재 열린 괄호를 저장할 스택임
+
+#     for i in s:
+#         # 여는 괄호일 경우 스택에 열린 괄호 추가
+#         if i == '[':
+#             tree.append('[')
+#             continue
+#         # 닫는 괄호일 경우
+#         max_depth = max(len(tree), max_depth)
+#         # 현재 깊이와 최대 깊이 중 큰 값을 저장하고
+#         tree.pop()  # 마지막 열린 괄호를 제거
+
+#     print(2 ** max_depth)  # 쌍을 이룰때 가지 생성, 가지2-.1개씩 나눠가짐
+#     # 결국엔 2의 depth승 
