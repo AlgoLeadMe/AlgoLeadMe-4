937790bd27d417e35f4afbcfd245395ad1bfb10a
jung0115 <jungim7490@gmail.com>
2024-11-02 15:15:17 +0900

    2024-11-04

---
+++ jung0115/그리디알고리즘/Programmers_42885.py
@@ -0,0 +1,21 @@
+# 23차시 2024.11.02.토 : 프로그래머스 - 구명보트(Lv.2)
+from collections import deque
+
+def solution(people, limit):
+    answer = 0
+    people.sort(reverse = True)
+    queue = deque(people)
+    
+    while len(queue) > 1 :
+      if queue[0] + queue[-1] <= limit :
+        queue.pop()
+        queue.popleft()
+        answer += 1
+      else :
+        queue.popleft()
+        answer += 1
+    
+    if len(queue) > 0 :
+      answer += 1
+    
+    return answer
